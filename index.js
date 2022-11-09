d5241568af59caae212283842a70956a9b5aba25
NeyamulOmy <neyamul.omy@gmail.com>
2022-11-09 14:59:13 +0600

    added /services

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ async function run() {
     try {
         const servicesCollection = client.db('ghorer-khabar').collection('servicesCollection');
 
+        app.get('/services3', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
