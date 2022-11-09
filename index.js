da706b949a05471a0e6dfbce5e08c59d5d2d5a49
NeyamulOmy <neyamul.omy@gmail.com>
2022-11-09 11:05:58 +0600

    reading data from database

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+const uri = "mongodb+srv://ghorer-khabar-admin:0Dbf9aeQBXBgTSrV@cluster0.bogje7w.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollection = client.db('ghorer-khabar').collection('servicesCollection');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run();
+
 app.get('/', (req, res) => {
     res.send('server running...')
 })
