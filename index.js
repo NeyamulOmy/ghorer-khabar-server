69ac3703e43d9a626322a822a73f72b89604b4be
NeyamulOmy <neyamul.omy@gmail.com>
2022-11-11 00:33:55 +0600

    added post, patch

--- index.js
+++ index.js
@@ -41,6 +41,53 @@ async function run() {
             res.send(review);
         });
 
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const cursor = reviewCollection.find(query).sort({ _id: -1 });
+            const review = await cursor.toArray();
+            res.send(review);
+        });
+
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.send(result);
+        });
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        app.patch('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const message = req.body.message
+            console.log(message)
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    message: message
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
 
     }
     finally {
